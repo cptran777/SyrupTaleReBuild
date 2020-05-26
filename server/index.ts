3aa33864912fdd04a2daa90dc4263c4ab2732a77
cptran777 <cptran14@gmail.com>
2020-05-25 23:38:14 -0700

    Init basic server index and update scripts for heroku

---
+++ server/index.ts
@@ -0,0 +1,33 @@
+"use strict";
+/****************************** INIT DEPENDENCIES ******************************/
+import express from 'express';
+import bodyparser from 'body-parser';
+import socketIO from 'socket.io';
+import http from 'http';
+
+require('dotenv').config();
+
+// init server application using express
+const app = express();
+
+// /***************************** INIT CUSTOM MODULES *****************************/
+
+// /******************************* INIT MIDDLEWARE *******************************/
+
+// // Should allow us to use the assets inside the public folder in the client side codes
+app.use(express.static(__dirname + "/client/build/"));
+app.use(bodyparser.json());
+
+// /********************************* INIT SERVER *********************************/
+
+const server = http.createServer(app);
+const io = socketIO(server);
+const port = process.env.PORT || 3001;
+
+io.on('connection', (socket) => {
+  socket.on('disconnect', () => console.log('Hello darkness my old friend'));
+});
+
+server.listen(port, () => {
+  console.log("Listening on port", port);
+});
